f66bb9b4d4e4334d747aedb43f196b360a14c122
César Aguilera <cesar.aguilera.p@gmail.com>
2017-10-10 21:50:24 +0100

    Canvas also supports drawing of several rectangles

--- src/test/java/cs4r/labs/drawingprogram/CanvasTest.java
+++ src/test/java/cs4r/labs/drawingprogram/CanvasTest.java
@@ -349,4 +349,41 @@ public class CanvasTest {
         assertThat(currentCanvas).isEqualTo(expectedCanvas);
     }
 
+    @Test
+    public void drawSeveralRectangles() throws Exception {
+
+        int height = 16;
+        int width = 16;
+
+        Canvas canvas = new Canvas(height, width);
+
+        canvas.drawRectangle(0, 0, 2, 2);
+        canvas.drawRectangle(3, 6, 9, 10);
+        canvas.drawRectangle(4, 13, 15, 15);
+
+        String currentCanvas = canvas.toText();
+
+        String expectedCanvas = ""
+                + "------------------\n"
+                + "|xxx             |\n"
+                + "|x x             |\n"
+                + "|xxx             |\n"
+                + "|                |\n"
+                + "|                |\n"
+                + "|                |\n"
+                + "|   xxxxxxx      |\n"
+                + "|   x     x      |\n"
+                + "|   x     x      |\n"
+                + "|   x     x      |\n"
+                + "|   xxxxxxx      |\n"
+                + "|                |\n"
+                + "|                |\n"
+                + "|    xxxxxxxxxxxx|\n"
+                + "|    x          x|\n"
+                + "|    xxxxxxxxxxxx|\n"
+                + "------------------\n";
+
+        assertThat(currentCanvas).isEqualTo(expectedCanvas);
+    }
+
 }
