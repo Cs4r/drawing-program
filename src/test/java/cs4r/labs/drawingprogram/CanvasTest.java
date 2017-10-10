73087bdbf776d8c98e73f1b21421cfdeccc914a5
César Aguilera <cesar.aguilera.p@gmail.com>
2017-10-10 20:10:19 +0100

    Apparently we can draw several horizontal lines on the canvas

--- src/test/java/cs4r/labs/drawingprogram/CanvasTest.java
+++ src/test/java/cs4r/labs/drawingprogram/CanvasTest.java
@@ -78,4 +78,35 @@ public class CanvasTest {
 
         assertThat(currentCanvas).isEqualTo(expectedCanvas);
     }
+
+    @Test
+    public void drawSeveralHorizontalLines() throws Exception {
+
+        int height = 7;
+        int width = 16;
+
+        Canvas canvas = new Canvas(height, width);
+
+        canvas.drawHorizontalLine(0, 3, 9);
+        canvas.drawHorizontalLine(1, 3, 6);
+        canvas.drawHorizontalLine(2, 3, 5);
+        canvas.drawHorizontalLine(4, 0, 4);
+        canvas.drawHorizontalLine(5, 0, 15);
+
+        String currentCanvas = canvas.toText();
+
+        String expectedCanvas = ""
+                + "------------------\n"
+                + "|   xxxxxxx      |\n"
+                + "|   xxxx         |\n"
+                + "|   xxx          |\n"
+                + "|                |\n"
+                + "|xxxxx           |\n"
+                + "|xxxxxxxxxxxxxxxx|\n"
+                + "|                |\n"
+                + "------------------\n";
+
+        assertThat(currentCanvas).isEqualTo(expectedCanvas);
+    }
+
 }
