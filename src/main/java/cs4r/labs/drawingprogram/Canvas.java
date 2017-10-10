198bd20713fb1c5a4a84088c4fd6fa1f517ce093
César Aguilera <cesar.aguilera.p@gmail.com>
2017-10-10 19:51:10 +0100

    We can now create an empty canvas

---
+++ src/main/java/cs4r/labs/drawingprogram/Canvas.java
@@ -0,0 +1,72 @@
+package cs4r.labs.drawingprogram;
+
+
+public class Canvas {
+
+    private int width;
+    private int height;
+    private char[][] canvas;
+
+    public Canvas(int height, int width) {
+        this.height = height;
+        this.width = width;
+        canvas = new char[this.height][this.width];
+        initCanvas();
+    }
+
+    /**
+     * Returns a text representation of this canvas.
+     * <p>
+     * <b>Note/b>: use {@link #toString()} only for debugging purposes.
+     *
+     * @return text representation of the canvas as {@link String}.
+     */
+    public String toText() {
+
+        StringBuilder canvasAsText = new StringBuilder();
+
+        addHeader(canvasAsText);
+
+        addCanvas(canvasAsText);
+
+        addFooter(canvasAsText);
+
+        return canvasAsText.toString();
+    }
+
+
+    private void initCanvas() {
+        for (int i = 0; i < height; ++i) {
+            for (int j = 0; j < width; ++j) {
+                canvas[i][j] = ' ';
+            }
+        }
+    }
+
+    private void addHeader(StringBuilder canvasAsText) {
+        addHorizontalBorder(canvasAsText);
+    }
+
+    private void addCanvas(StringBuilder canvasAsText) {
+        for (int i = 0; i < height; ++i) {
+            canvasAsText.append('|');
+            for (int j = 0; j < width; ++j) {
+                canvasAsText.append(canvas[i][j]);
+            }
+            canvasAsText.append('|');
+            canvasAsText.append('\n');
+        }
+    }
+
+    private void addFooter(StringBuilder canvasAsText) {
+        addHorizontalBorder(canvasAsText);
+    }
+
+    private void addHorizontalBorder(StringBuilder canvasAsText) {
+        for (int i = 0; i < width + 2; ++i) {
+            canvasAsText.append('-');
+        }
+        canvasAsText.append('\n');
+    }
+
+}
---
+++ src/test/java/cs4r/labs/drawingprogram/CanvasTest.java
@@ -0,0 +1,33 @@
+package cs4r.labs.drawingprogram;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+
+/**
+ * Unit tests for {@link Canvas}
+ */
+public class CanvasTest {
+
+    @Test
+    public void createEmptyCanvas() throws Exception {
+
+        int height = 4;
+        int width = 10;
+
+        Canvas canvas = new Canvas(height, width);
+
+        String currentCanvas = canvas.toText();
+
+        String expectedCanvas = ""
+                + "------------\n"
+                + "|          |\n"
+                + "|          |\n"
+                + "|          |\n"
+                + "|          |\n"
+                + "------------\n";
+
+        assertThat(currentCanvas).isEqualTo(expectedCanvas);
+    }
+}
