2177f0ad6b9d5594cac960a9d3f98d691471546d
César Aguilera <cesar.aguilera.p@gmail.com>
2017-10-14 14:54:48 +0100

    DrawRectangle can draw a rectangle if the context is active and there is a canvas in the context

--- src/main/java/cs4r/labs/drawingprogram/commandimpl/DrawRectangle.java
+++ src/main/java/cs4r/labs/drawingprogram/commandimpl/DrawRectangle.java
@@ -1,5 +1,6 @@
 package cs4r.labs.drawingprogram.commandimpl;
 
+import cs4r.labs.drawingprogram.Canvas;
 import cs4r.labs.drawingprogram.CommandImplementation;
 import cs4r.labs.drawingprogram.DrawingContext;
 import cs4r.labs.drawingprogram.util.Checks;
@@ -9,9 +10,25 @@ import cs4r.labs.drawingprogram.util.Checks;
  */
 public class DrawRectangle implements CommandImplementation {
 
-    @Override
-    public void execute(String arguments, DrawingContext drawingContext) {
-        Checks.failIfAnyArgumentIsNull(arguments, drawingContext);
+    private final ArgumentParser argumentParser;
 
+    public DrawRectangle(ArgumentParser argumentParser) {
+
+        this.argumentParser = argumentParser;
+    }
+
+    @Override
+    public void execute(String arguments, DrawingContext context) {
+        Checks.failIfAnyArgumentIsNull(arguments, context);
+
+        context.isActive();
+        Canvas canvas = context.getCanvas().get();
+
+        Integer x1 = argumentParser.getPositionalArgument(arguments, 0, Integer.class) - 1;
+        Integer y1 = argumentParser.getPositionalArgument(arguments, 1, Integer.class) - 1;
+        Integer x2 = argumentParser.getPositionalArgument(arguments, 2, Integer.class) - 1;
+        Integer y2 = argumentParser.getPositionalArgument(arguments, 3, Integer.class) - 1;
+
+        canvas.drawRectangle(x1, y1, x2, y2);
     }
 }
--- src/test/java/cs4r/labs/drawingprogram/commandimpl/DrawRectangleTest.java
+++ src/test/java/cs4r/labs/drawingprogram/commandimpl/DrawRectangleTest.java
@@ -1,20 +1,34 @@
 package cs4r.labs.drawingprogram.commandimpl;
 
+import cs4r.labs.drawingprogram.Canvas;
 import cs4r.labs.drawingprogram.DrawingContext;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.*;
 
 /**
  * Unit tests for {@link DrawRectangle}.
  */
+@RunWith(MockitoJUnitRunner.class)
 public class DrawRectangleTest {
+
+    @Mock
+    private DrawingContext context;
+    @Mock
+    private ArgumentParser argumentParser;
+    @Mock
+    private Canvas canvas;
 
     @Test
     public void requireNonNullArguments() throws Exception {
 
-        DrawRectangle drawRectangle = new DrawRectangle();
+        DrawRectangle drawRectangle = new DrawRectangle(argumentParser);
 
         assertThatThrownBy(() -> drawRectangle.execute(null, mock(DrawingContext.class)))
                 .isInstanceOf(IllegalArgumentException.class)
@@ -23,5 +37,40 @@ public class DrawRectangleTest {
         assertThatThrownBy(() -> drawRectangle.execute(null, mock(DrawingContext.class)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessage("arguments cannot be null");
+    }
+
+    @Test
+    public void drawRectangle() throws Exception {
+
+        // Given
+        activeContextWithCanvas();
+        String arguments = "1 1 40 40";
+        validArguments(arguments, 1, 1, 40, 40);
+        DrawRectangle drawRectangle = new DrawRectangle(argumentParser);
+
+        // When
+        drawRectangle.execute(arguments, context);
+
+        // Then
+        verify(context).isActive();
+        verify(context).getCanvas();
+        verify(argumentParser).getPositionalArgument(arguments, 0, Integer.class);
+        verify(argumentParser).getPositionalArgument(arguments, 1, Integer.class);
+        verify(argumentParser).getPositionalArgument(arguments, 2, Integer.class);
+        verify(argumentParser).getPositionalArgument(arguments, 3, Integer.class);
+        verify(canvas).drawRectangle(0, 0, 39, 39);
+    }
+
+    private void validArguments(String arguments, int x1, int y1, int x2, int y2) {
+        argumentParser = mock(ArgumentParser.class);
+        when(argumentParser.getPositionalArgument(arguments, 0, Integer.class)).thenReturn(x1);
+        when(argumentParser.getPositionalArgument(arguments, 1, Integer.class)).thenReturn(y1);
+        when(argumentParser.getPositionalArgument(arguments, 2, Integer.class)).thenReturn(x2);
+        when(argumentParser.getPositionalArgument(arguments, 3, Integer.class)).thenReturn(y2);
+    }
+
+    private void activeContextWithCanvas() {
+        when(context.isActive()).thenReturn(true);
+        when(context.getCanvas()).thenReturn(Optional.of(canvas));
     }
 }
